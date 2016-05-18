7927ddd40a6430c0365d37c826ee12da359ee993
Matthew D. Steele <mdsteele@alum.mit.edu>
2016-05-18 13:31:25 -0400

    Add LICENSE and README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tuna
+
+Image editor for ASCII Hex Image (.ahi) files, a simple text-based image format
+for 16-color images.  See the [ahi](https://github.com/mdsteele/ahi) repository
+for more information about the format.
+
+## License
+
+_Tuna_ is licensed under the GNU GPL, version 3.  _Tuna_ is free software: you
+can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+_Tuna_ is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+The complete license can be found in the LICENSE file.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/*=============================================================================
+| Copyright 2016 Matthew D. Steele <mdsteele@alum.mit.edu>                    |
+|                                                                             |
+| This file is part of Tuna.                                                  |
+|                                                                             |
+| Tuna is free software: you can redistribute it and/or modify it under the   |
+| terms of the GNU General Public License as published by the Free Software   |
+| Foundation, either version 3 of the License, or (at your option) any later  |
+| version.                                                                    |
+|                                                                             |
+| Tuna is distributed in the hope that it will be useful, but WITHOUT ANY     |
+| WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   |
+| FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more      |
+| details.                                                                    |
+|                                                                             |
+| You should have received a copy of the GNU General Public License along     |
+| with Tuna.  If not, see <http://www.gnu.org/licenses/>.                     |
+=============================================================================*/
+
 // TODO:
 // - Image resizing
 // - Open-file/Save-as
